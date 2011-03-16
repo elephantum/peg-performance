b17ee292a0628dece86fe355cc2e8bd525129bc5
Andrey Tatarinov <elephantum@yandex.ru>
2011-03-16 17:33:35 -0400

    *

--- src/main/python/performance.py
+++ src/main/python/performance.py
@@ -33,8 +33,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import collections
-
 # Move: (fromh, jumped, to)
 # Coordinate: (row, hole)
 
